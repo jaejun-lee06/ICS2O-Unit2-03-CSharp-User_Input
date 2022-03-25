0e489a8db13307902b7549e9f113e57cf6f99f65
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-03-25 17:28:04 +0000

    extra

--- main.cs
+++ main.cs
@@ -1,7 +1,29 @@
+// Created by: Jaejun Lee
+// Created on: Mar 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        string firstName;
+        int age;
+
+        Console.WriteLine("This program gets a user's address.");
+        Console.WriteLine("");
+
+        Console.Write("Enter your street name: ");
+        firstName = Console.ReadLine();
+        Console.Write("Enter your street number: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("Your address is: " + age + " " + firstName + ".");
+
+        Console.WriteLine("\nDone.");
+    }
 }
